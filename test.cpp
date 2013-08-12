37e193329e1b8aac381213d793afb906ba9c3724
Holden Li <holden.null@gmail.com>
2013-08-11 23:56:00 -0400

    added SDL, display frames in window as fast as possible

--- test.cpp
+++ test.cpp
@@ -5,6 +5,8 @@ extern "C" {
 #include "libswscale/swscale.h"
 }
 
+#include "SDL/SDL.h"
+
 #include "lodepng.h"
 #include <iostream>
 #include <sstream>
@@ -22,11 +24,22 @@ void encodeOneStep(const char* filename, const unsigned char* image, unsigned wi
 
 int main(int argc, char* args[])
 {
-    (void)argc;
-    (void)args;
+    // Setup
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER))
+    {
+        std::cerr << "ERROR: SDL init failed - " << SDL_GetError() << std::endl;
+        exit(1);
+    }
 
+    if (argc < 2)
+    {
+        std::cerr << "ERROR: Wrong number of args" << std::endl;
+        exit(1);
+    }
+    
     av_register_all();
 
+    // Open File
     AVFormatContext *format_context = NULL;
 
     if (avformat_open_input(&format_context, args[1], NULL, NULL) != 0)
@@ -42,7 +55,6 @@ int main(int argc, char* args[])
     }
 
     av_dump_format(format_context, 0, args[1], 0);
-
     
     // Find the video stream
     int video_stream = -1;
@@ -60,6 +72,8 @@ int main(int argc, char* args[])
         return -1;
     }
     AVCodecContext *codec_context = format_context->streams[video_stream]->codec;
+    int vwidth = codec_context->width;
+    int vheight = codec_context->height;
 
     // Decode
     AVCodec *codec = avcodec_find_decoder(codec_context->codec_id);
@@ -80,17 +94,31 @@ int main(int argc, char* args[])
     {
         return -1;
     }
+    
+    // Setup screen
+    SDL_Surface * screen = SDL_SetVideoMode(vwidth, vheight, 0, 0);
+    if (!screen)
+    {
+        std::cerr << "ERROR: SDL init failed - " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+
+    SDL_Overlay * bmp = SDL_CreateYUVOverlay(vwidth, vheight, SDL_YV12_OVERLAY, screen);
 
     // Create 24 bit RGB image
     int num_bytes = avpicture_get_size(DST_FORMAT,
-        codec_context->width, codec_context->height);
+        vwidth, vheight);
     uint8_t *buffer = (uint8_t*)av_malloc(num_bytes * sizeof(uint8_t));
     avpicture_fill((AVPicture *)frameRGB, buffer, DST_FORMAT,
-        codec_context->width, codec_context->height);
-    SwsContext *sws_context = sws_getCachedContext(NULL,
-        codec_context->width, codec_context->height, codec_context->pix_fmt,
-        codec_context->width, codec_context->height, DST_FORMAT,
-        SWS_BICUBIC, 0, 0, 0
+        vwidth, vheight);
+    SwsContext *sws_context = sws_getCachedContext(
+        NULL,
+        vwidth, vheight,
+        codec_context->pix_fmt,
+        vwidth, vheight,
+        PIX_FMT_YUV420P,
+        SWS_BICUBIC,
+        0, 0, 0
         );
 
     // Read data
@@ -107,12 +135,7 @@ int main(int argc, char* args[])
 
             if (frame_finished)
             {
-                sws_scale(
-                    sws_context, frame->data, frame->linesize,
-                    0, codec_context->height,
-                    frameRGB->data, frameRGB->linesize
-                    );
-
+                /*
                 // Save frame
                 if (++i > 0)
                 {
@@ -122,12 +145,55 @@ int main(int argc, char* args[])
                     ss << i;;
                     ss << ".png";
                     std::string str = ss.str();
-                    encodeOneStep(str.c_str(), buffer, codec_context->width, codec_context->height);
+                    encodeOneStep(str.c_str(), buffer, vwidth, vheight);
+                    
                 }
+                */
+                SDL_LockYUVOverlay(bmp);
+
+                AVPicture pict;
+                pict.data[0] = bmp->pixels[0];
+                pict.data[1] = bmp->pixels[2];
+                pict.data[2] = bmp->pixels[1];
+
+                pict.linesize[0] = bmp->pitches[0];
+                pict.linesize[1] = bmp->pitches[2];
+                pict.linesize[2] = bmp->pitches[1];
+                
+                sws_scale(
+                    sws_context,
+                    frame->data,
+                    frame->linesize,
+                    0,
+                    vheight,
+                    pict.data,
+                    pict.linesize
+                    );
+
+                SDL_UnlockYUVOverlay(bmp);
+                SDL_Rect rect;
+                rect.x = 0;
+                rect.y = 0;
+                rect.w = vwidth;
+                rect.h = vheight;
+                SDL_DisplayYUVOverlay(bmp, &rect);
             }
         }
 
         av_free_packet(&packet);
+
+        // Poll Events
+        SDL_Event event;
+        SDL_PollEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                SDL_Quit();
+                exit(0);
+                break;
+            default:
+                break;
+        }
     }
 
     // Done
